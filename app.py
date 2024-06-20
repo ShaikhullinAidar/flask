3bdd157a555412001d9f5a5901c7ebe0cba93f9e
ShaikhullinAidar <shaikhullin.aidar@gmail.com>
2024-06-20 23:34:36 +0500

    update app

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def main_page():
+    return '<h1>Hello from Flask & Docker!</h2>'
 
 
 if __name__ == "__main__":
