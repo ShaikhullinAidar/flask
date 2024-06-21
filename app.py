d395602916106252d5400c35a9949009e1abffab
ShaikhullinAidar <shaikhullin.aidar@gmail.com>
2024-06-21 12:07:14 +0500

    update app

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return '<h1>Hello from Flask & Docker! \n OLD VERSION</h2>'
+    a = random.randint(0, 100)
+    b = random.randint(0, 100)
+    c = random.randint(0, 100)
+    d = random.randint(0, 100)
+    return f'<h1>Реши примеры</h2>' \
+           f'<p>{a} + {b} =</p>' \
+           f'<p>{c} - {d} =</p>'
 
 
 if __name__ == "__main__":
